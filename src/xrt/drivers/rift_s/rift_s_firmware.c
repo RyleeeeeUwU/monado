f1da233255b39c5bcc91472d9e112bc8ff722c9e
Jan Schmidt <jan@centricular.com>
2022-04-20 00:26:05 +1000

    d/rift_s: Port json parsing to cJSON

--- src/xrt/drivers/rift_s/rift_s_firmware.c
+++ src/xrt/drivers/rift_s/rift_s_firmware.c
@@ -7,266 +7,228 @@
 
 /* Oculus Rift S Driver - firmware JSON parsing functions */
 #include <string.h>
+#include <stdio.h>
+
+#include "util/u_json.h"
+#include "util/u_misc.h"
 
 #include "rift_s_firmware.h"
-#include "../ext_deps/nxjson.h"
 
-static bool
-json_read_vec3(const nx_json *nxj, const char *key, vec3f *out)
-{
-	const nx_json *member = nx_json_get(nxj, key);
-
-	if (member->type != NX_JSON_ARRAY)
-		return false;
-
-	for (int i = 0; i < 3; i++) {
-		const nx_json *item = nx_json_item(member, i);
-
-		if (item->type != NX_JSON_DOUBLE)
-			return false;
-
-		out->arr[i] = item->dbl_value;
-	}
-
-	return true;
-}
-
-static bool
-json_read_mat4x4(const nx_json *nxj, const char *key, mat4x4f *out)
-{
-	const nx_json *member = nx_json_get(nxj, key);
-
-	if (member->type != NX_JSON_ARRAY)
-		return false;
-
-	for (int i = 0; i < 12; i++) {
-		const nx_json *item = nx_json_item(member, i);
-
-		if (item->type != NX_JSON_DOUBLE)
-			return false;
-
-		out->arr[i] = item->dbl_value;
-	}
-
-	return true;
-}
-
-static bool
-json_read_mat3x3(const nx_json *nxj, const char *key, float out[3][3])
-{
-	const nx_json *member = nx_json_get(nxj, key);
-	int i = 0;
-
-	if (member->type != NX_JSON_ARRAY)
-		return false;
-
-	for (int y = 0; y < 3; y++) {
-		for (int x = 0; x < 3; x++) {
-			const nx_json *item = nx_json_item(member, i);
-
-			if (item->type != NX_JSON_DOUBLE)
-				return false;
-
-			out[y][x] = item->dbl_value;
-			i++;
-		}
-	}
-
-	return true;
-}
+#define JSON_INT(a, b, c) u_json_get_int(u_json_get(a, b), c)
+#define JSON_FLOAT(a, b, c) u_json_get_float(u_json_get(a, b), c)
+#define JSON_DOUBLE(a, b, c) u_json_get_double(u_json_get(a, b), c)
+#define JSON_VEC3(a, b, c) u_json_get_vec3_array(u_json_get(a, b), c)
+#define JSON_MATRIX_3X3_ARRAY(a, b, c) u_json_get_float_array(u_json_get(a, b), c.v, 9)
+#define JSON_MATRIX_4x4_ARRAY(a, b, c) u_json_get_float_array(u_json_get(a, b), c.v, 16)
 
 int
-rift_s_parse_imu_calibration(char *json, rift_s_imu_calibration *c)
+rift_s_parse_imu_calibration(char *json_string, rift_s_imu_calibration *c)
 {
-	const nx_json *nxj, *obj, *version, *imu;
+	const cJSON *obj, *version, *imu;
 	float version_number = -1;
 
-	nxj = nx_json_parse(json, 0);
-	if (nxj == NULL)
+	cJSON *json_root = cJSON_Parse(json_string);
+	if (!cJSON_IsObject(json_root)) {
+		RIFT_S_ERROR("Could not parse JSON IMU calibration data.");
+		cJSON_Delete(json_root);
 		return -1;
+	}
 
-	obj = nx_json_get(nxj, "FileFormat");
-	if (obj->type == NX_JSON_NULL)
-		goto fail;
-
-	version = nx_json_get(obj, "Version");
-	if (version->type != NX_JSON_STRING) {
+	obj = u_json_get(json_root, "FileFormat");
+	if (!cJSON_IsObject(json_root)) {
 		goto fail;
 	}
-	version_number = strtof(version->text_value, NULL);
+
+	version = u_json_get(obj, "Version");
+	char *version_str = cJSON_GetStringValue(version);
+	if (version_str == NULL) {
+		goto fail;
+	}
+	version_number = strtof(version_str, NULL);
 	if (version_number != 1.0)
 		goto fail;
 
-	imu = nx_json_get(nxj, "ImuCalibration");
-	if (obj->type == NX_JSON_NULL)
+	imu = u_json_get(json_root, "ImuCalibration");
+	if (!cJSON_IsObject(imu)) {
+		goto fail;
+	}
+
+	if (!JSON_MATRIX_4x4_ARRAY(imu, "DeviceFromImu", c->imu_to_device_transform))
 		goto fail;
 
-	if (!json_read_mat4x4(imu, "DeviceFromImu", &c->imu_to_device_transform))
+	obj = u_json_get(imu, "Gyroscope");
+	if (!cJSON_IsObject(obj) || !JSON_MATRIX_3X3_ARRAY(obj, "RectificationMatrix", c->gyro.rectification)) {
 		goto fail;
+	}
 
-	obj = nx_json_get(imu, "Gyroscope");
-	if (obj->type != NX_JSON_OBJECT)
+	obj = u_json_get(obj, "Offset");
+	if (!cJSON_IsObject(obj) || !JSON_VEC3(obj, "ConstantOffset", &c->gyro.offset)) {
 		goto fail;
+	}
 
-	if (!json_read_mat3x3(obj, "RectificationMatrix", c->gyro.rectification))
+	obj = u_json_get(imu, "Accelerometer");
+	if (!cJSON_IsObject(obj) || !JSON_MATRIX_3X3_ARRAY(obj, "RectificationMatrix", c->accel.rectification)) {
 		goto fail;
+	}
 
-	obj = nx_json_get(obj, "Offset");
-	if (obj->type != NX_JSON_OBJECT)
+	obj = u_json_get(obj, "Offset");
+	if (!cJSON_IsObject(obj) || !JSON_VEC3(obj, "OffsetAtZeroDegC", &c->accel.offset_at_0C) ||
+	    !JSON_VEC3(obj, "OffsetTemperatureCoefficient", &c->accel.temp_coeff)) {
 		goto fail;
+	}
 
-	if (!json_read_vec3(obj, "ConstantOffset", &c->gyro.offset))
-		goto fail;
-
-	obj = nx_json_get(imu, "Accelerometer");
-	if (obj->type != NX_JSON_OBJECT)
-		goto fail;
-
-	if (!json_read_mat3x3(obj, "RectificationMatrix", c->accel.rectification))
-		goto fail;
-
-	obj = nx_json_get(obj, "Offset");
-	if (obj->type != NX_JSON_OBJECT)
-		goto fail;
-
-	if (!json_read_vec3(obj, "OffsetAtZeroDegC", &c->accel.offset_at_0C))
-		goto fail;
-	if (!json_read_vec3(obj, "OffsetTemperatureCoefficient", &c->accel.temp_coeff))
-		goto fail;
-
-	nx_json_free(nxj);
+	cJSON_Delete(json_root);
 	return 0;
 
 fail:
-	LOGW("Unrecognised Rift S IMU Calibration JSON data. Version %f\n%s\n", version_number, json);
-	nx_json_free(nxj);
+	RIFT_S_WARN("Unrecognised Rift S IMU Calibration JSON data. Version %f\n%s\n", version_number, json_string);
+	cJSON_Delete(json_root);
 	return -1;
 }
 
 static bool
-json_read_led_point(const nx_json *led_model, rift_s_led *led, int n)
+json_read_led_point(const cJSON *led_model, rift_s_led *led, int n)
 {
-	const nx_json *array;
-	const nx_json *point[9];
+	const cJSON *array;
+	double point[9];
 	char name[32];
-	int j;
 
 	snprintf(name, 32, "Point%d", n);
-	array = nx_json_get(led_model, name);
-	if (array->type != NX_JSON_ARRAY || array->length != 9) {
+	array = u_json_get(led_model, name);
+	if (!cJSON_IsArray(array) || cJSON_GetArraySize(array) != 9) {
 		return false;
 	}
 
-	for (j = 0; j < 9; j++)
-		point[j] = nx_json_item(array, j);
+	int j = 0;
+	const cJSON *item = NULL;
+	cJSON_ArrayForEach(item, array)
+	{
+		if (!cJSON_IsNumber(item)) {
+			return false;
+		}
+		point[j++] = item->valuedouble;
+	}
 
-	led->pos.x = point[0]->dbl_value;
-	led->pos.y = point[1]->dbl_value;
-	led->pos.z = point[2]->dbl_value;
-	led->dir.x = point[3]->dbl_value;
-	led->dir.y = point[4]->dbl_value;
-	led->dir.z = point[5]->dbl_value;
-	led->angles.x = point[6]->dbl_value;
-	led->angles.y = point[7]->dbl_value;
-	led->angles.z = point[8]->dbl_value;
+	led->pos.x = point[0];
+	led->pos.y = point[1];
+	led->pos.z = point[2];
+	led->dir.x = point[3];
+	led->dir.y = point[4];
+	led->dir.z = point[5];
+	led->angles.x = point[6];
+	led->angles.y = point[7];
+	led->angles.z = point[8];
 
 	return true;
 }
 
 static bool
-json_read_lensing_model(const nx_json *lensing_model, rift_s_lensing_model *model, int n)
+json_read_lensing_model(const cJSON *lensing_model, rift_s_lensing_model *model, int n)
 {
-	const nx_json *array;
+	const cJSON *array;
 	char name[32];
-	int j;
 
 	snprintf(name, 32, "Model%d", n);
-	array = nx_json_get(lensing_model, name);
-	if (array->type != NX_JSON_ARRAY || array->length != 5) {
+	array = u_json_get(lensing_model, name);
+	if (!cJSON_IsArray(array) || cJSON_GetArraySize(array) != 5) {
 		return false;
 	}
 
-	model->num_points = nx_json_item(array, 0)->int_value;
+	model->num_points = cJSON_GetArrayItem(array, 0)->valueint;
 
-	for (j = 0; j < 4; j++)
-		model->points[j] = nx_json_item(array, j + 1)->dbl_value;
+	for (int j = 0; j < 4; j++) {
+		const cJSON *item = cJSON_GetArrayItem(array, j + 1);
+		if (!cJSON_IsNumber(item)) {
+			return false;
+		}
+
+		model->points[j] = item->valuedouble;
+	}
 
 	return true;
 }
 
 int
-rift_s_controller_parse_imu_calibration(char *json, rift_s_controller_imu_calibration *c)
+rift_s_controller_parse_imu_calibration(char *json_string, rift_s_controller_imu_calibration *c)
 {
-	const nx_json *nxj, *obj, *version, *leds;
+	const cJSON *obj, *version, *leds;
+	const cJSON *item = NULL;
 	int i;
 
-	nxj = nx_json_parse(json, 0);
-	if (nxj == NULL)
+	cJSON *json_root = cJSON_Parse(json_string);
+	if (!cJSON_IsObject(json_root)) {
+		RIFT_S_ERROR("Could not parse JSON Controller IMU calibration data.");
+		cJSON_Delete(json_root);
 		return -1;
-
-	obj = nx_json_get(nxj, "TrackedObject");
-	if (obj->type == NX_JSON_NULL)
-		goto fail;
-
-	version = nx_json_get(obj, "FlsVersion");
-	if (version->type != NX_JSON_STRING) {
-		goto fail;
 	}
-	if (strcmp(version->text_value, "1.0.10")) {
-		LOGE("Controller calibration version number has changed - got %s", version->text_value);
+
+	obj = u_json_get(json_root, "TrackedObject");
+	if (!cJSON_IsObject(obj)) {
 		goto fail;
 	}
 
-	if (!json_read_vec3(obj, "ImuPosition", &c->imu_position))
+	version = u_json_get(obj, "FlsVersion");
+	char *version_str = cJSON_GetStringValue(version);
+	if (version_str == NULL || strcmp(version_str, "1.0.10")) {
+		RIFT_S_ERROR("Controller calibration version number has changed - got %s", version_str);
+		goto fail;
+	}
+
+	if (!JSON_VEC3(obj, "ImuPosition", &c->imu_position))
 		goto fail;
 
-	if (!json_read_mat4x4(obj, "AccCalibration", &c->accel_calibration))
+	if (!JSON_MATRIX_4x4_ARRAY(obj, "AccCalibration", c->accel_calibration))
 		goto fail;
 
-	if (!json_read_mat4x4(obj, "GyroCalibration", &c->gyro_calibration))
+	if (!JSON_MATRIX_4x4_ARRAY(obj, "GyroCalibration", c->gyro_calibration))
 		goto fail;
 
 	/* LED positions */
-	leds = nx_json_get(obj, "ModelPoints");
-	if (leds->type != NX_JSON_OBJECT)
+	leds = u_json_get(obj, "ModelPoints");
+	if (!cJSON_IsObject(leds)) {
 		goto fail;
+	}
 
-	c->num_leds = leds->length;
+	/* FIXME: Array foreach */
+	c->num_leds = cJSON_GetArraySize(leds);
 	c->leds = calloc(c->num_leds, sizeof(rift_s_led));
-	for (i = 0; i < c->num_leds; i++) {
+	i = 0;
+	cJSON_ArrayForEach(item, leds)
+	{
+		i++;
 		if (!json_read_led_point(leds, c->leds + i, i))
 			goto fail;
 	}
 
 	/* LED lensing models */
-	leds = nx_json_get(obj, "Lensing");
-	if (leds->type != NX_JSON_OBJECT)
+	leds = u_json_get(obj, "Lensing");
+	if (!cJSON_IsObject(leds)) {
 		goto fail;
+	}
 
-	c->num_lensing_models = leds->length;
+	c->num_lensing_models = cJSON_GetArraySize(leds);
 	c->lensing_models = calloc(c->num_lensing_models, sizeof(rift_s_lensing_model));
-	for (i = 0; i < c->num_lensing_models; i++) {
+	i = 0;
+	cJSON_ArrayForEach(item, leds)
+	{
 		if (!json_read_lensing_model(leds, c->lensing_models + i, i))
 			goto fail;
 	}
 
-	if (!json_read_mat3x3(nxj, "gyro_m", c->gyro.rectification))
+	if (!JSON_MATRIX_3X3_ARRAY(json_root, "gyro_m", c->gyro.rectification) ||
+	    !JSON_VEC3(json_root, "gyro_b", &c->gyro.offset) ||
+	    !JSON_MATRIX_3X3_ARRAY(json_root, "acc_m", c->gyro.rectification) ||
+	    !JSON_VEC3(json_root, "acc_b", &c->gyro.offset)) {
 		goto fail;
-	if (!json_read_vec3(nxj, "gyro_b", &c->gyro.offset))
-		goto fail;
+	}
 
-	if (!json_read_mat3x3(nxj, "acc_m", c->accel.rectification))
-		goto fail;
-	if (!json_read_vec3(nxj, "acc_b", &c->accel.offset))
-		goto fail;
-
-	nx_json_free(nxj);
+	cJSON_Delete(json_root);
 	return 0;
 
 fail:
-	LOGW("Unrecognised Rift S Controller Calibration JSON data.\n%s\n", json);
+	RIFT_S_WARN("Unrecognised Rift S Controller Calibration JSON data.\n%s\n", json_string);
 	rift_s_controller_free_imu_calibration(c);
-	nx_json_free(nxj);
+	cJSON_Delete(json_root);
 	return -1;
 }
 
