bf9e14a712173e550c274909ecb4bdfeb71d8bf2
Joel-Valenciano <joelv1907@gmail.com>
2025-03-14 21:12:22 -0500

    a/tracking: Start Renaming t_tracker_psvr2_fusion

--- src/xrt/auxiliary/tracking/t_tracker_kalman_fusion_c.cpp
+++ src/xrt/auxiliary/tracking/t_tracker_kalman_fusion_c.cpp
@@ -1,69 +1,56 @@
-// Copyright 2024, Joel Valenciano
-// Copyright 2025, Beyley Cardellio
-// SPDX-License-Identifier: BSL-1.0
-/*!
- * @file
- * @brief  C interface to generalized kalman filter.
- *
- * @author Joel Valenciano <joelv1907@gmail.com>
- * @author Beyley Cardellio <ep1cm1n10n123@gmail.com>
- * @ingroup aux_tracking
- */
-
-#include "t_tracker_kalman_fusion.hpp"
+#include "tracking/t_tracker_psvr2_fusion.hpp"
 #include "xrt/xrt_tracking.h"
-
 #include <cstdlib>
+#include "psvr2_fusion.h"
 
-using xrt::auxiliary::tracking::KalmanFusionInterface;
+using xrt::auxiliary::tracking::PSVR2FusionInterface;
 
-struct KalmanFusionInterfaceWrapper
+struct PSVR2FusionInterfaceWrapper
 {
-	std::unique_ptr<KalmanFusionInterface> fusion;
+	std::unique_ptr<PSVR2FusionInterface> fusion;
 
-	KalmanFusionInterfaceWrapper() : fusion(KalmanFusionInterface::create()) {}
+	PSVR2FusionInterfaceWrapper() : fusion(PSVR2FusionInterface::create()) {}
 
-	~KalmanFusionInterfaceWrapper() {}
+	~PSVR2FusionInterfaceWrapper() {}
 };
 
 extern "C" {
 
-struct KalmanFusionInterfaceWrapper *
-kalman_fusion_create(void)
+struct PSVR2FusionInterfaceWrapper *
+psvr2_fusion_create(void)
 {
-	return new KalmanFusionInterfaceWrapper;
+	return new PSVR2FusionInterfaceWrapper;
 }
 
 void
-kalman_fusion_destroy(KalmanFusionInterfaceWrapper *wrapper)
+psvr2_fusion_destroy(PSVR2FusionInterfaceWrapper *wrapper)
 {
 	delete wrapper;
 }
 
 void
-kalman_fusion_process_imu_data(KalmanFusionInterfaceWrapper *wrapper,
-                               struct xrt_imu_sample *sample,
-                               const struct xrt_vec3 *accel_variance_optional,
-                               const struct xrt_vec3 *gyro_variance_optional)
+psvr2_fusion_process_imu_data(PSVR2FusionInterfaceWrapper *wrapper,
+                              struct xrt_imu_sample *sample,
+                              struct xrt_vec3 *orientation_variance_optional)
 {
-	wrapper->fusion->process_imu_data(sample, accel_variance_optional, gyro_variance_optional);
+	wrapper->fusion->process_imu_data(sample, orientation_variance_optional);
 }
 
 void
-kalman_fusion_process_pose(KalmanFusionInterfaceWrapper *wrapper,
-                           struct xrt_pose_sample *sample,
-                           struct xrt_vec3 *position_variance_optional,
-                           struct xrt_vec3 *orientation_variance_optional,
-                           float residual_limit)
+psvr2_fusion_process_slam_pose(PSVR2FusionInterfaceWrapper *wrapper,
+                               struct xrt_pose_sample *sample,
+                               struct xrt_vec3 *position_variance_optional,
+                               struct xrt_vec3 *orientation_variance_optional,
+                               float residual_limit)
 {
-	wrapper->fusion->process_pose(sample, position_variance_optional, orientation_variance_optional,
-	                              residual_limit);
+	wrapper->fusion->process_slam_pose(sample, position_variance_optional, orientation_variance_optional,
+	                                   residual_limit);
 }
 
 void
-kalman_fusion_get_prediction(struct KalmanFusionInterfaceWrapper *wrapper,
-                             timepoint_ns timestamp_ns,
-                             struct xrt_space_relation *out_relation)
+psvr2_fusion_get_prediction(struct PSVR2FusionInterfaceWrapper *wrapper,
+                            timepoint_ns timestamp_ns,
+                            struct xrt_space_relation *out_relation)
 {
 	wrapper->fusion->get_prediction(timestamp_ns, out_relation);
 }
